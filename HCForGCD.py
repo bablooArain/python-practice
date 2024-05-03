66cc85d668de9f0fae1f3fce6d820ca16e10931a
Nabeel Aslam <babluj099@gmail.com>
2024-05-03 15:02:15 +0500

    modified hcf

--- HCForGCD.py
+++ HCForGCD.py
@@ -1,11 +1,2 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-devisor = 2
-while(devisor):
-    if(num1%devisor==0) and (num2%devisor==0):
-        print("Here's the devisor of both: ", devisor)
-        break
-    elif(devisor > num1) or (devisor > num2):
-        print("No devisor found")
-        break
-    devisor = devisor + 1
