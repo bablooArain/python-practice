2ea76e3cf25fbdecbf729c5250bfc96e9a4517dd
Nabeel Aslam <babluj099@gmail.com>
2024-05-23 14:46:49 +0500

    modified file

--- HCForGCD.py
+++ HCForGCD.py
@@ -1,2 +1,13 @@
-num1 = int(input("Enter first number: "))
+num1 = int(input("Enter first second number: "))
 num2 = int(input("Enter second number: "))
+if(num1 >= num2):
+    divisor = num1 -1 
+else:
+    divisor = num2 -1    
+while(1):
+    if(num1 % divisor == 0 and num2 % divisor == 0):
+        print("The GCD is: ",divisor)
+        break
+    divisor -= 1
+    if(divisor == 1):
+        break
